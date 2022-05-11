f7ca74eceefe09dde0fcd36cd61558591363d615
yeontan0826 <yeontan0826@gmail.com>
2022-05-11 19:01:35 +0900

    조건문 : 온도

---
+++ programmers/09.js
@@ -0,0 +1,10 @@
+/* 조건문 : 온도 */
+function temperature(num) {
+    if(num <= 18) {
+        console.log("조금 춥네요");
+    } else if(num <= 23) {
+        console.log("날씨가 좋네요");
+    } else {
+        console.log("조금 덥습니다");
+    }
+}
