d601e663bf196c0e60d420f7c94d7e32658e881e
yeontan0826 <yeontan0826@gmail.com>
2022-05-11 19:01:09 +0900

    조건문 : 조건문 연습

---
+++ programmers/07.js
@@ -0,0 +1,8 @@
+/* 조건문 : 조건문 연습 */
+function boolean(input1, input2) {
+    if(input1 === true || input2 === true) {
+        console.log("true");
+    } else {
+        console.log("false");
+    }
+}
