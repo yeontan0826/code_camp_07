0f738c7839dc1e7e4417b86dd41e1063a1ad656e
yeontan0826 <yeontan0826@gmail.com>
2022-05-16 14:25:10 +0900

    container & presenter

---
+++ class/pages/06-01-container-presenter/index.js
@@ -0,0 +1,9 @@
+import BoardWrite from "../../src/components/units/board/write/BoardWrite.container"
+
+const ContainerPresenterPage = () => {
+    return(
+        <BoardWrite />
+    )
+}
+
+export default ContainerPresenterPage
