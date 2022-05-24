7125ded9733483db46f434ab6f7abc81819b6288
yeontan0826 <yeontan0826@gmail.com>
2022-05-24 11:39:39 +0900

    apply prettier space 4

--- class/pages/_app.tsx
+++ class/pages/_app.tsx
@@ -1,20 +1,20 @@
 import "../styles/globals.css";
+import "antd/dist/antd.css";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import "antd/dist/antd.css"
 import { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const client = new ApolloClient({
-    uri: "http://example.codebootcamp.co.kr/graphql",
-    cache: new InMemoryCache(),
-  });
+    const client = new ApolloClient({
+        uri: "http://example.codebootcamp.co.kr/graphql",
+        cache: new InMemoryCache(),
+    });
 
-  return (
-    // 모든 페이지에서 사용할 수 있게끔 <ApolloProvider></ApolloProvider>로 컴포넌트를 감싼다
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
-  );
+    return (
+        // 모든 페이지에서 사용할 수 있게끔 <ApolloProvider></ApolloProvider>로 컴포넌트를 감싼다
+        <ApolloProvider client={client}>
+            <Component {...pageProps} />
+        </ApolloProvider>
+    );
 }
 
 export default MyApp;
