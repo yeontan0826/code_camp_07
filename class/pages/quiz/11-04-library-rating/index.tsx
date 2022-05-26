25babd617a8bd6050a1f2515809758282066ac51
yeontan0826 <yeontan0826@gmail.com>
2022-05-26 20:01:16 +0900

    eslint-disable react/prop-types

--- class/pages/quiz/11-04-library-rating/index.tsx
+++ class/pages/quiz/11-04-library-rating/index.tsx
@@ -1,5 +1,10 @@
+/* eslint-disable react/prop-types */
 import styled from "@emotion/styled"
 import { useState } from "react"
+
+interface IStarProps {
+    isActive: boolean
+}
 
 const starPoint = [1, 2, 3, 4, 5]
 
@@ -18,7 +23,7 @@ const LibraryRating = () => {
     const Star = styled.div`
         width: 50px;
         height: 50px;
-        background-image: url(${(props: {active: boolean}) => props.active ? "/image/11-04-library-rating/ic_star_purple.svg" : "/image/11-04-library-rating/ic_star_gray.svg"});
+        background-image: url(${(props: IStarProps) => props.isActive ? "/image/11-04-library-rating/ic_star_purple.svg" : "/image/11-04-library-rating/ic_star_gray.svg"});
         background-repeat: no-repeat;
         background-position: center;
     `
@@ -28,7 +33,7 @@ const LibraryRating = () => {
             <Wrapper>
                 {starPoint.map((num) => {
                     return (
-                        <Star active={num <= rating} onClick={() => onClickStar(num)} key={num}/>
+                        <Star isActive={(num <= rating)} onClick={() => onClickStar(num)} key={num}/>
                     )
                 })}
             </Wrapper>
