1311b4ef4f051bb223245b2635c4ab71b54687fe
yeontan0826 <yeontan0826@gmail.com>
2022-06-02 14:45:25 +0900

    create layout for quiz

--- class/src/quiz/onetwothree/components/commons/layout/banner/index.tsx
+++ class/src/quiz/onetwothree/components/commons/layout/banner/index.tsx
@@ -4,52 +4,52 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
 const Wrapper = styled.div`
-    height: 200px;
-    background-color: pink;
+  height: 200px;
+  background-color: pink;
 `;
 
 const SlideBox = styled.div`
-    width: 300px;
-    margin: auto;
+  width: 300px;
+  margin: auto;
 `;
 
 const SlideImg = styled.img`
-    height: 150px;
-    margin: auto;
+  height: 150px;
+  margin: auto;
 `;
 
 const LayoutBanner = () => {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-    };
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
 
-    return (
-        <Wrapper>
-            <SlideBox>
-                <Slider {...settings}>
-                    <div>
-                        <SlideImg src="/image/puppy/puppy1.jpg" />
-                    </div>
-                    <div>
-                        <SlideImg src="/image/puppy/puppy2.jpeg" />
-                    </div>
-                    <div>
-                        <SlideImg src="/image/puppy/puppy3.jpeg" />
-                    </div>
-                    <div>
-                        <SlideImg src="/image/puppy/puppy4.jpeg" />
-                    </div>
-                    <div>
-                        <SlideImg src="/image/puppy/puppy5.jpeg" />
-                    </div>
-                </Slider>
-            </SlideBox>
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <SlideBox>
+        <Slider {...settings}>
+          <div>
+            <SlideImg src="/image/puppy/puppy1.jpg" />
+          </div>
+          <div>
+            <SlideImg src="/image/puppy/puppy2.jpeg" />
+          </div>
+          <div>
+            <SlideImg src="/image/puppy/puppy3.jpeg" />
+          </div>
+          <div>
+            <SlideImg src="/image/puppy/puppy4.jpeg" />
+          </div>
+          <div>
+            <SlideImg src="/image/puppy/puppy5.jpeg" />
+          </div>
+        </Slider>
+      </SlideBox>
+    </Wrapper>
+  );
 };
 
 export default LayoutBanner;
