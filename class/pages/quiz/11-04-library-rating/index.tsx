35b437d09cd44a8b3499fb23052dd57583abce6f
yeontan0826 <yeontan0826@gmail.com>
2022-06-02 14:41:26 +0900

    create rating library

--- class/pages/quiz/11-04-library-rating/index.tsx
+++ class/pages/quiz/11-04-library-rating/index.tsx
@@ -1,45 +1,42 @@
 /* eslint-disable react/prop-types */
-import styled from "@emotion/styled"
-import { useState } from "react"
+import styled from "@emotion/styled";
+import { useState } from "react";
 
 interface IStarProps {
-    isActive: boolean
+  isActive: boolean;
 }
 
-const starPoint = [1, 2, 3, 4, 5]
+const starPoint = [1, 2, 3, 4, 5];
 
 const LibraryRating = () => {
+  const [rating, setRating] = useState(0);
 
-    const [rating, setRating] = useState(0)
+  const onClickStar = (num: number) => {
+    setRating(num);
+  };
 
-    const onClickStar = (num: number) => {
-        setRating(num)
-    }
+  const Wrapper = styled.div`
+    display: flex;
+  `;
 
-    const Wrapper = styled.div`
-        display: flex;     
-    `
+  const Star = styled.div`
+    width: 50px;
+    height: 50px;
+    background-image: url(${(props: IStarProps) => (props.isActive ? "/image/11-04-library-rating/ic_star_purple.svg" : "/image/11-04-library-rating/ic_star_gray.svg")});
+    background-repeat: no-repeat;
+    background-position: center;
+  `;
 
-    const Star = styled.div`
-        width: 50px;
-        height: 50px;
-        background-image: url(${(props: IStarProps) => props.isActive ? "/image/11-04-library-rating/ic_star_purple.svg" : "/image/11-04-library-rating/ic_star_gray.svg"});
-        background-repeat: no-repeat;
-        background-position: center;
-    `
+  return (
+    <>
+      <Wrapper>
+        {starPoint.map((num) => {
+          return <Star isActive={num <= rating} onClick={() => onClickStar(num)} key={num} />;
+        })}
+      </Wrapper>
+      <div>{rating}점</div>
+    </>
+  );
+};
 
-    return(
-        <>
-            <Wrapper>
-                {starPoint.map((num) => {
-                    return (
-                        <Star isActive={(num <= rating)} onClick={() => onClickStar(num)} key={num}/>
-                    )
-                })}
-            </Wrapper>
-            <div>{rating}점</div>
-        </>
-    )
-}
-
-export default LibraryRating
+export default LibraryRating;
