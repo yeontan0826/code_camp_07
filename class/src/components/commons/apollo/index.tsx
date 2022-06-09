e87c26112a5977add78ef1631b7c2fd0f1c1c3d4
yeontan0826 <yeontan0826@gmail.com>
2022-06-09 16:22:26 +0900

    pre-rendering 실습

--- class/src/components/commons/apollo/index.tsx
+++ class/src/components/commons/apollo/index.tsx
@@ -1,12 +1,37 @@
 import { ApolloClient, ApolloLink, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
+import { useEffect } from "react";
 import { useRecoilState } from "recoil";
 import { accessTokenState } from "../../../commons/store";
 
 const ApolloSetting = (props: any) => {
-	// const [isEdit, setIsEdit] = useState(false);
+	const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
 
-	const [accessToken] = useRecoilState(accessTokenState);
+	// 1. 프리렌더링 예제 - process.browser 방법
+	// if (process.browser) {
+	// 	console.log("지금은 브라우저다!!!");
+	// 	localStorage.getItem("accessToken");
+	// } else {
+	// 	console.log("지금은 프론트엔드 서버다!! yarn dev 프로그램이다!!");
+	// 	localStorage.getItem("accessToken");
+	// }
+
+	// 2. 프리렌더링 예제 - typeof window 방법
+	// if (typeof window !== "undefined") {
+	// 	console.log("지금은 브라우저다!!!");
+	// 	localStorage.getItem("accessToken");
+	// } else {
+	// 	console.log("지금은 프론트엔드 서버다!! yarn dev 프로그램이다!!");
+	// 	localStorage.getItem("accessToken");
+	// }
+
+	// 3. 프리렌더링 예제 - useEffect 방법
+	useEffect(() => {
+		console.log("지금은 브라우저다!!!");
+		console.log(localStorage.getItem("accessToken"));
+		const accessToken = localStorage.getItem("accessToken");
+		setAccessToken(accessToken || "");
+	}, []);
 
 	const uploadLink = createUploadLink({
 		uri: "http://backend07.codebootcamp.co.kr/graphql",
