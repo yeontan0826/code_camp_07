6a19ae911503b6340409ad4eb3fe492da31d7017
yeontan0826 <yeontan0826@gmail.com>
2022-06-30 13:21:57 +0900

    docker settings

--- class_build/next.config.js
+++ class_build/next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true
+  trailingSlash: true,
+  generateBuildId: () => "freemarket"
 }
 
 module.exports = nextConfig
